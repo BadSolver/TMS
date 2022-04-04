69bf8cc113391d6a4dd5d098497dc26c0e6d25f1
Alex Kurov <twistter8@gmail.com>
2022-04-04 12:48:36 +0300

    Add HW JS

---
+++ JS HW 15/app.js
@@ -0,0 +1,87 @@
+//Task 1
+let a = 20;
+let b = 58;
+let c = 42;
+
+let result = a + b + c;
+
+console.log(result);
+
+//Task 2
+
+const secondInMinute = 60;
+const minuteInHour = 60;
+const hoursInDay = 24;
+const daysInYear = 365;
+
+let myAgeInSecond = secondInMinute * minuteInHour * hoursInDay * daysInYear * 27;
+
+console.log(myAgeInSecond);
+
+//Task 3 
+
+let count = 42;
+let userName = '42';
+
+count = String(count);
+userName = Number(userName);
+console.log(typeof count);
+console.log(typeof userName);
+
+
+//Task 4
+
+let a = 1;
+let b = 2;
+let c = ' белых медведей';
+a = String(a);
+b = String(b);
+ const result = a + b + c;
+ console.log(result);
+
+
+ //Task 5 
+
+ let access = 'доступ';
+     marine = 'Морпех';
+     frost = 'наледь';
+     across = 'поперек';
+     chopped = 'рубило';
+
+ let wordLength = access.length + marine.length + frost.length + across.length + chopped.length;
+
+ console.log(wordLength);
+
+ //Task 6 
+ let str = 'Это просто текст';
+ `Variable: str have type: ${typeof(str)}`
+ let num = 10;
+ `Variable: num have type: ${typeof(num)}`
+ let autumn = false;
+ `Variable: autumn have type: ${typeof(autumn)}`
+
+
+ //Task 7
+ let name = prompt('как тебя зовут?', '');
+ let age = prompt('сколько тебе лет? ', '')
+ console.log('Меня зовут ' + name + ' мне ' + age + 'лет');
+
+
+ //Task 1 AL
+ let a = 4;
+ let b = 3;
+
+ a = 3;
+ b = 4;
+
+ //Task 2 AL   
+ let codeWord1 = "обернись";
+ let codeWord2 = "неужели";
+ let codeWord3 = "огурцы";
+ let codeWord4 = "липкие";
+ let codeWord5 = "?!"; 
+
+ let cipher = codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1];
+ console.log(cipher)
+
+
