24371331b7d26c37681d7006cb0c338e6c30e05b
Alex Kurov <Alex>
2022-04-18 12:59:41 +0300

    add HW 17

---
+++ JS17/app (2).js
@@ -0,0 +1,100 @@
+// Task 1
+
+
+// const getSum = (number) => {
+//     let sum = 0;
+//     for(i = 0; i <= number; i++) {
+//         sum = sum + i;
+//     }
+//     return sum
+// }
+// console.log(getSum(100))
+
+
+// Task2
+
+// const credit = (number) => {
+//     let percent = (number / 5 + ((number - number / 5) * 0.17));
+//     return percent
+// }
+// console.log(credit(15000))
+
+
+ // Task 3
+
+// const trimString = (string, a , b) => {
+//     let str = string.slice(a , b);
+//     return str
+// }
+// console.log(trimString('My name is Alex', 1, 9))
+
+// Task 4
+
+// let getSumNumbers = (number) => {
+//     let array = String(number).split('').reduce(function(a, b){
+//         return +a + +b;
+//     })
+//     return array
+// }
+// console.log(getSumNumbers(2021121127 ))
+
+
+
+// Task 5
+
+
+// const getSum = (a , b) => {
+//     let value = 0;
+//     for (i = a; i <= b; i++) {
+//         value = value + i;
+//     }
+//     return value
+// }
+// console.log(getSum(-1, 4))
+
+
+// Task 6 
+
+// const foo = () => {
+//     console.log('foo')
+// }
+// const boo = () => {
+//     console.log('boo')
+// }
+// const boolFooBoo = (bool, foo, boo) => {
+//     if(bool){
+//          foo()
+//     }else if(bool === false) {
+//       boo()
+//     }else if(bool === undefined){
+//         console.log('FooBoo')
+//     }
+// }
+// boolFooBoo(undefined, foo, boo)
+
+
+
+
+
+
+// const foo = () => {
+//     console.log('foo')
+// }
+// const boo = () => {
+//     console.log('boo')
+// }
+
+// const foboo = (boole, foo, boo) => {
+//     if(boole) { //true false
+//         foo()
+//     }else{
+//         boo()
+//     }
+
+// }
+// foboo(false, foo, boo)
+
+
+
+
+
